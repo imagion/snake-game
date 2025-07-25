bc800a381de554a02506b458c3e7fe2e92d6e8bb
imagion <algernoncowperwood@gmail.com>
2025-07-25 17:05:09 +0300

    Состояния змейки и еды

--- app/(pages)/grid/page.tsx
+++ app/(pages)/grid/page.tsx
@@ -1,9 +1,16 @@
 'use client';
 
+import { cn } from '@/lib/utils';
+import { useState } from 'react';
+
 export default function GridPage() {
+  const [snake, setSnake] = useState<Array<number>>([10, 11, 12]);
+  const [food, setFood] = useState<number>(15);
+
   // Размер сетки: количество строк и колонок
   const rows = 20;
   const cols = 20;
+  console.log(Array.from({ length: rows * cols }).map((_, index) => index));
 
   return (
     <div className='flex h-screen items-center justify-center bg-gray-100 dark:bg-gray-900'>
@@ -20,7 +27,11 @@ export default function GridPage() {
         {Array.from({ length: rows * cols }).map((_, index) => (
           <div
             key={index}
-            className='border border-gray-200 bg-white dark:border-gray-700 dark:bg-gray-800'
+            className={cn(
+              'border border-gray-200 bg-white dark:border-gray-700 dark:bg-gray-800',
+              snake.includes(index) ? 'bg-green-500' : '',
+              food.includes(index) ? 'bg-red-500' : '',
+            )}
           />
         ))}
       </div>
