01bdd39674e7d5bb22e615caf6bb33e69a5c37b4
imagion <algernoncowperwood@gmail.com>
2025-07-27 14:20:16 +0300

    Проверка на поедание и перемещение еды

--- app/(pages)/grid/page.tsx
+++ app/(pages)/grid/page.tsx
@@ -9,12 +9,10 @@ export default function GridPage() {
   const [direction, setDirection] = useState<'right' | 'left' | 'up' | 'down'>(
     'right',
   );
-
-  // Размер сетки: количество строк и колонок
   const rows = 20;
   const cols = 20;
 
-  // Функция для вычисления следующего хода змейки
+  // Вычисляем следующий индекс головы
   const getNextHead = (currentHead: number, direction: string): number => {
     switch (direction) {
       case 'right':
@@ -30,15 +28,51 @@ export default function GridPage() {
     }
   };
 
-  // Функция для движения змейки
+  // TODO:Телепортация за границами
+  const wrapAround = (index: number): number => {
+    // Получаем координаты головы
+    let row = Math.floor(index / cols);
+    let col = index % cols;
+
+    // Проверяем, не выходит ли голова за границы сетки
+    if (col < 0) col = cols - 1;
+    if (col >= cols) col = 0;
+    if (row < 0) row = rows - 1;
+    if (row >= rows) row = 0;
+
+    return row * cols + col;
+  };
+
+  // Получаем случайную свободную клетку
+  const getRandomFreeCell = (occupied: number[]): number => {
+    const maxIndex = rows * cols - 1;
+    let rand: number;
+    do {
+      rand = Math.floor(Math.random() * (maxIndex + 1));
+    } while (occupied.includes(rand));
+    return rand;
+  };
+
+  // Движение змейки
   const moveSnake = () => {
     setSnake((prev) => {
-      const newHead = getNextHead(prev[0], direction);
-      const newSnake = [newHead, ...prev.slice(0, -1)];
+      const newHead = wrapAround(getNextHead(prev[0], direction));
+      let newSnake: number[];
+
+      if (newHead === food) {
+        // поедание, добавляем голову, не удаляем хвост, перемещаем еду
+        newSnake = [newHead, ...prev];
+        setFood(getRandomFreeCell(newSnake));
+      } else {
+        // обычный ход, обрезаем хвост
+        newSnake = [newHead, ...prev.slice(0, -1)];
+      }
+
       return newSnake;
     });
   };
 
+  // Автодвижение
   useEffect(() => {
     const interval = setInterval(() => {
       moveSnake();
